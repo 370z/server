d74ef17c2eb01b9bfc59361ac3655a7cb06e77b8
intelligent.farming.platform@gmail.com <intelligent.farming.platform@gmail.com>
2020-10-07 14:03:27 +0700

    server v.1

---
+++ api/users/user.service.js
@@ -0,0 +1,88 @@
+const pool = require("../../config/database");
+
+module.exports = {
+  create: (data, callBack) => {
+    pool.query(
+      `insert into products(userName, password, email, status_level) 
+                values(?,?,?,?)`,
+      [
+        data.userName,
+        data.password,
+        data.email,
+        data.status_level
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  getUserByUserEmail: (email, callBack) => {
+    pool.query(
+      `select * from products where email = ?`,
+      [email],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUserByUserId: (id, callBack) => {
+    pool.query(
+      `select id,userName,password,email,status_level from products where id = ?`,
+      [id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  getUsers: callBack => {
+    pool.query(
+      `select id,userName,password,email,status_level from products`,
+      [],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results);
+      }
+    );
+  },
+  updateUser: (data, callBack) => {
+    pool.query(
+      `update products set userName=?, password=?, email=?, status_level=? where id = ?`,
+      [
+        data.userName,
+        data.password,
+        data.email,
+        data.status_level,
+        data.id
+      ],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  },
+  deleteUser: (data, callBack) => {
+    pool.query(
+      `delete from products where id = ?`,
+      [data.id],
+      (error, results, fields) => {
+        if (error) {
+          callBack(error);
+        }
+        return callBack(null, results[0]);
+      }
+    );
+  }
+};
