1c48e10145b49314a96695e87200fc41ab6b00aa
370z <mos-moji350@hotmail.com>
2020-10-08 15:35:07 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
 const userRouter = require("./api/users/user.router");
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/users", userRouter);
